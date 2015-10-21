ab5baaee23d0f4f24f7b314f05b022c06df11cf8
Vitaly Tarasenko <vetal.tarasenko@gmail.com>
2015-10-21 20:43:24 +0300

    RF: use better naming for classes in tests

--- spec/modules/non_shared_accessors_spec.rb
+++ spec/modules/non_shared_accessors_spec.rb
@@ -5,52 +5,52 @@ describe NonSharedAccessors do
   context 'Native Class Accessor in Ruby' do
 
     it 'should explain native behavior (shared class variable)' do
-      class A
+      class A1
         cattr_accessor :value
       end
 
-      class B < A
+      class B1 < A1
 
       end
 
-      expect(A.value).to be_nil
-      expect(B.value).to be_nil
+      expect(A1.value).to be_nil
+      expect(B1.value).to be_nil
 
-      A.value = 2
+      A1.value = 2
 
-      expect(A.value).to eq(2)
-      expect(B.value).to eq(2)
+      expect(A1.value).to eq(2)
+      expect(B1.value).to eq(2)
 
-      B.value = 3
+      B1.value = 3
 
-      expect(A.value).to eq(3)
-      expect(B.value).to eq(3)
+      expect(A1.value).to eq(3)
+      expect(B1.value).to eq(3)
     end
   end
 
   it 'should behave as a separate accessor(separate class valiable)' do
 
-    class A
+    class A2
       include NonSharedAccessors
       non_shared_cattr_accessor :value
     end
 
-    class B < A
+    class B2 < A2
 
     end
 
-    expect(A.value).to be_nil
-    expect(B.value).to be_nil
+    expect(A2.value).to be_nil
+    expect(B2.value).to be_nil
 
-    A.value = 2
+    A2.value = 2
 
-    expect(A.value).to eq(2)
-    expect(B.value).to eq(nil)
+    expect(A2.value).to eq(2)
+    expect(B2.value).to eq(nil)
 
-    B.value = 3
+    B2.value = 3
 
-    expect(A.value).to eq(2)
-    expect(B.value).to eq(3)
+    expect(A2.value).to eq(2)
+    expect(B2.value).to eq(3)
   end
 
 end
